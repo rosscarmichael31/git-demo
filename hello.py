86d59ff05da9085d555bb7e7a0ac62020638f239
Ross Carmichael <Ross.Carmichael31@thehutgroup.com>
2022-09-27 10:57:02 +0100

    Added Jupyter feature

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
+import sys 
+
+
 def hello():
+	if sys.argv[1] == "Jupyter":
+		hellojupyter()
+	else:
+		helloworld()	
+
+def hellojupyter():
+	print("Hello, Jupyter!")
+
+def helloworld():
 	print("Hello, World!")
 
 if __name__ == '__main__':
